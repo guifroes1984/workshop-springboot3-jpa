39172671270111ad39b565ba3bfb33c7bb533a60
guifroes1984 <guilhermefroes26@gmail.com>
2023-07-25 11:15:26 -0300

    Projeto criado

---
+++ src/main/java/br/com/guifroes1984/curso/CursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.guifroes1984.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+}
